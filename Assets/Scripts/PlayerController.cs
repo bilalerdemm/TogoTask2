f52c127eafc0366caedfb964afc9295c9e3c2ea7
bilalerdemm <77274317+bilalerdemm@users.noreply.github.com>
2022-12-24 17:21:41 +0300

    Win ve Lose Olayları

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.UI;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject collectPoint;
+    public GameObject collectPoint, blueTable;
     public int maxChildCount;
     public SkinnedMeshRenderer playerMesh;
-
+    public int score = 0;
+    public Text scoreText;
+    public GameObject winPanel, lostPanel;
+    bool isFinish = false;
     private void Start()
     {
+
+    }
+    private void Update()
+    {
+        scoreText.text = score.ToString();
+        if (score < 0)
+        {
+            lostPanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (isFinish)
+        {
+            if (transform.childCount == 3)
+            {
+                winPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
 
     }
 
@@ -18,8 +40,6 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("BlueTable"))
         {
-            Debug.Log("Blue Table");
-
             int childCount = Random.Range(1, maxChildCount);
             maxChildCount -= maxChildCount;
             if (this.transform.childCount < 4)
@@ -27,23 +47,38 @@ public class PlayerController : MonoBehaviour
                 playerMesh.material.color = other.gameObject.transform.GetChild(childCount).GetComponent<MeshRenderer>().material.color;
                 other.gameObject.transform.GetChild(childCount).transform.parent = transform;
                 this.transform.GetChild(3).transform.position = collectPoint.transform.position;
+                if (other.gameObject.transform.childCount == 1)
+                {
+                    isFinish = true;
+                }
             }
-
-
         }
-        if (transform.childCount==4)
+        if (transform.childCount == 4)
         {
-
-
             if (other.gameObject.CompareTag("GreenTable"))
             {
                 transform.GetChild(3).transform.parent = other.gameObject.transform;
-
+                if (other.gameObject.transform.GetChild(other.gameObject.transform.childCount - 1).tag == ("GreenBall"))
+                {
+                    score = score + 10;
+                }
+                else
+                {
+                    score = score - 5;
+                }
             }
 
             if (other.gameObject.CompareTag("RedTable"))
             {
                 transform.GetChild(3).transform.parent = other.gameObject.transform;
+                if (other.gameObject.transform.GetChild(other.gameObject.transform.childCount - 1).tag == ("RedBall"))
+                {
+                    score = score + 10;
+                }
+                else
+                {
+                    score = score - 5;
+                }
             }
         }
     }
